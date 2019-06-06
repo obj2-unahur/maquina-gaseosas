bcfb1a44f5fd6a6514fa611f23e738f70b75c525
Darío Hruszecki <dhruszecki@despegar.com>
2019-06-06 11:44:13 -0300

    inicial

---
+++ src/test/java/ar/edu/unahur/obj2/MainTest.java
@@ -0,0 +1,17 @@
+package ar.edu.unahur.obj2;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+}
